805cca65f27388ff0b79b1f0388c8c826852e8aa
Renan Florez <renan@cloudwalk.io>
2021-03-15 22:48:01 -0300

    finally we are able to deblock and remove the rdw

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,45 @@
+use std::convert::TryFrom;
 use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::io::{self, BufRead};
+use std::io::{Cursor, Read, Seek, SeekFrom};
 
-fn inspect(content: &Vec<u8>, full_string: bool) {
-    if full_string {
-        println!("{:?}", content);
-        println!("{}", String::from_utf8_lossy(content));
-    } else {
-        let mut i = 0;
-        for x in content {
-            i += 1;
-            println!("{}, {}, {}", *x, *x as char, i);
-        }
-    }
+// Each subsequent byte has a potential value of 255 (because it's in ASCII)
+// so a RDW of 0u8 0u8 1u8 3u8 actually means that the RDW refers to the next 258 characters
+// (0 × 255³) + (0 × 255²) + (1 × 255¹) + (3 × 255⁰) = 258
+fn rdw_to_size(rdw_buffer: &[u8]) -> usize {
+    let s: u64 = rdw_buffer
+        .iter()
+        .enumerate()
+        .map(|(index, rdw_number)| -> u64 {
+            let index_translation = i8::abs(i8::try_from(index).unwrap() - 3i8);
+            let index_power: u64 = 255u64.pow(u32::try_from(index_translation).unwrap());
+            u64::try_from(*rdw_number).unwrap() * index_power
+        })
+        .sum();
+    usize::try_from(s).unwrap()
 }
 
 fn main() -> std::io::Result<()> {
     let f = File::open("./tt067t0")?;
-    let mut reader = BufReader::new(f);
 
-    let mut line: Vec<u8> = Vec::new();
+    // removing at signs
+    let mut big_line: Vec<u8> = f.bytes().map(|x| x.unwrap()).collect();
+    big_line.retain(|byte| *byte != b'@');
 
-    let len = reader.read_to_end(&mut line)?;
+    let mut position: usize = 0;
+    while big_line.len() > position {
+        //checking for EOF
+        if &big_line[position] != &0u8 {
+            position += 1;
+        };
 
-    let cursor = io::Cursor::new(&line);
-    let mut split_iter = cursor.split(0u8).map(|l| l.unwrap());
+        let rdw_slice = &big_line[position..position + 4];
+        let calculated_rdw = rdw_to_size(&rdw_slice);
 
-    while let Some(mut bytes) = split_iter.next() {
-        //TODO remove_rdw
-        //remove_at_signs(bytes);
-        //remove_slice(&mut bytes, &vec![b'@', b'@']) ;
-        bytes.retain(|byte| *byte != b'@');
+        position = position + 4;
 
-        if bytes.len() > 0 {
-            inspect(&bytes, true);
-        }
+        let content_slice = &big_line[position..(position + calculated_rdw)];
+        println!("{}", String::from_utf8_lossy(content_slice));
+        position = position + calculated_rdw;
     }
-
-    // TODO write the header extractor
-    // TODO extract it
-    println!("First line is {} bytes long", len);
-
     Ok(())
 }
